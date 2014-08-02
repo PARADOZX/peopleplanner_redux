cf5a08e300719f68cc4be852dcd7f2d0cbf8b88d
James Ling Chiang <jcc9283@hotmail.com>
2014-08-02 13:20:37 -0400

    commit 4

--- class/dbquery.php
+++ class/dbquery.php
@@ -4,17 +4,21 @@ include 'dbconnect.php'; //DEBUGGER
 
 class dbquery{
 	protected $DB = '';
-	protected $dummyVar = 8;  //month -- get this from AJAX data
+	protected $month;  
 
 	function __construct(PDO $connect){
+		//pass in DB connection
 		if ($this->DB == '') $this->DB = $connect;
+		//validate GET month var from AJAX call
+		$this->month = (isset($_GET['month']) && filter_var($_GET['month'], FILTER_VALIDATE_INT, array("options"=>
+		array("min_range"=>1, "max_range"=>12)))) ? $_GET['month'] : '';
 	}
 
 	public function getDate(){
 		// $q = "SELECT EXTRACT(DAY FROM date) as date, EXTRACT(MONTH FROM date) as month FROM date WHERE dateID = ?";
 		$q = "SELECT u.firstName, EXTRACT(DAY FROM d.date) as date FROM user as u INNER JOIN dateuser as du ON u.userID = du.userID INNER JOIN date as d ON d.dateID = du.dateID WHERE EXTRACT(MONTH FROM d.date) = ?";
 		$stmt = $this->DB->prepare($q);
-		$stmt->execute(array($this->dummyVar));
+		$stmt->execute(array($this->month));
 		$stmt->setFetchMode(PDO::FETCH_ASSOC);
 		$array = array();
 		// $array['info'] = '';
